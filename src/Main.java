42573265a8a95f19e87a02a13d9d118f9dadea14
AsiaIv0918 <104051067@live.asia.edu.tw>
2017-10-30 14:45:12 +0800

    NEW

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,25 @@
 import java.util.*;
 public class Main {
-
+//轉換成絕對值
     public static void main(String[] args) {
-	int a;
-	Scanner in=new Scanner(System.in);
-	a = in.nextInt();
-	if (a>=0)
+		Scanner in=new Scanner(System.in);
+//老師作法
+		int x;
+		x=in.nextInt();
+		if(x<0)
+			x=-x;
+		System.out.print(x);
+	   /*
+	   int a;
+	   a = in.nextInt();
+	    if (a>=0)
 	    System.out.printf("%d",a);
 		if (a<=0)
 			a=a*-1;
 			System.out.printf("%d",a);
+			*/
+
+
+
     }
 }
